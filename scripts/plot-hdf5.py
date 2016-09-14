3291749c9fc5d98bedb186e3d6b91ed1a4857d85
Patrik Schönfeldt <patrik.schoenfeldt@kit.edu>
2016-09-14 13:09:17 +0200

    plot-hdf5 now shows tracking data (if present)

--- scripts/plot-hdf5.py
+++ scripts/plot-hdf5.py
@@ -6,13 +6,14 @@ import numpy as np
 import h5py
 import sys
 
-def rms(arg,axis):
-	return np.sqrt(np.mean(np.square(arg),axis=axis))
-
 fname = sys.argv[1]
 
 hdf_f = h5py.File(fname, 'r')
+
+inovesa_version = hdf_f['/Info/INOVESA_v']
+
 current = (hdf_f['/Info/Parameters'].attrs['BunchCurrent'])
+grid_size = (hdf_f['/Info/Parameters'].attrs['GridSize'])
 time = hdf_f['/Info/AxisValues_t'][...]
 bunchlength = hdf_f['/BunchLength/data'][...]
 energyspread = hdf_f['/EnergySpread/data'][...]
@@ -33,5 +34,14 @@ h1, l1 = ax1.get_legend_handles_labels()
 h2, l2 = ax2.get_legend_handles_labels()
 ax1.legend(h1+h2, l1+l2,bbox_to_anchor=(0., 1.02, 1., .102), loc=3,
            ncol=3, mode="expand", borderaxespad=0.)
+
+
+if inovesa_version >= 13:
+    tracks = hdf_f['/Particles/data'][...]
+    plt.figure()
+    plt.axes().set_aspect('equal')
+    for i in range(tracks.shape[1]):
+        plt.plot(tracks[:,i,0]/grid_size,tracks[:,i,1]/grid_size)
+
 
 plt.show()
