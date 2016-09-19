e6a4555cff2234c7664a9f3be82dac36e8006e85
Patrik Schönfeldt <patrik.schoenfeldt@kit.edu>
2016-09-19 10:04:02 +0200

    Corrected copyright notices

--- inc/fixed_point.h
+++ inc/fixed_point.h
@@ -2,6 +2,7 @@
  * Inovesa - Inovesa Numerical Optimized Vlasov-Equation Solver Application   *
  * Copyright (c) 2007-2009: Peter Schregle (Fixed Point Math Library)         *
  * Copyright (c) 2014-2016: Patrik Schönfeldt                                 *
+ * Copyright (c) 2014-2016: Karlsruhe Institute of Technology                 *
  *                                                                            *
  * This file is part of Inovesa.                                              *
  * Inovesa is free software: you can redistribute it and/or modify            *
