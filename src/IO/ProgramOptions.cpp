a9343dff23f6f201370a814aa0537e6042452330
Patrik Schönfeldt <patrik.schoenfeldt@kit.edu>
2016-09-20 08:14:12 +0200

    Fixed: Did not compile w/o HDF5

--- src/IO/ProgramOptions.cpp
+++ src/IO/ProgramOptions.cpp
@@ -256,6 +256,7 @@ void vfps::ProgramOptions::save(std::string fname)
     }
 }
 
+#ifdef INOVESA_USE_HDF5
 void vfps::ProgramOptions::save(vfps::HDF5File* file)
 {
     for (po::variables_map::iterator it=_vm.begin(); it != _vm.end(); it++ ) {
@@ -276,3 +277,4 @@ void vfps::ProgramOptions::save(vfps::HDF5File* file)
         }
     }
 }
+#endif // INOVESA_USE_HDF5
