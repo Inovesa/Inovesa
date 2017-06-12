01a774e0edde3917677a3d16df85b593f01e6b3b
Patrik Schönfeldt <patrik.schoenfeldt@kit.edu>
2017-06-12 11:11:59 +0200

    Branched v0.14

--- inc/defines.hpp
+++ inc/defines.hpp
@@ -28,8 +28,8 @@
 #include "fixed_point.h"
 
 #define INOVESA_VERSION_RELEASE 0
-#define INOVESA_VERSION_MINOR   13
-#define INOVESA_VERSION_FIX     -2
+#define INOVESA_VERSION_MINOR   14
+#define INOVESA_VERSION_FIX     -1
 
 //#define INOVESA_SYNC_CL
 
