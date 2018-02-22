a5ad4e53a8cb62d49d3e2ae7a2ae4b988e994ab2
Patrik Schönfeldt <patrik.schoenfeldt@kit.edu>
2018-02-22 12:19:10 +0100

    Added class to display tracked electrons

---
+++ inc/IO/GUI/Plot2DPoints.hpp
@@ -0,0 +1,52 @@
+/******************************************************************************
+ * Inovesa - Inovesa Numerical Optimized Vlasov-Equation Solver Application   *
+ * Copyright (c) 2018: Patrik Schönfeldt                                      *
+ * Copyright (c) 2018: Karlsruhe Institute of Technology                      *
+ *                                                                            *
+ * This file is part of Inovesa.                                              *
+ * Inovesa is free software: you can redistribute it and/or modify            *
+ * it under the terms of the GNU General Public License as published by       *
+ * the Free Software Foundation, either version 3 of the License, or          *
+ * (at your option) any later version.                                        *
+ *                                                                            *
+ * Inovesa is distributed in the hope that it will be useful,                 *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of             *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              *
+ * GNU General Public License for more details.                               *
+ *                                                                            *
+ * You should have received a copy of the GNU General Public License          *
+ * along with Inovesa.  If not, see <http://www.gnu.org/licenses/>.           *
+ ******************************************************************************/
+
+#ifndef PLOT2DPOINTS_HPP
+#define PLOT2DPOINTS_HPP
+
+#include "IO/GUI/GUIElement.hpp"
+#include "PS/PhaseSpace.hpp"
+
+namespace vfps
+{
+
+class Plot2DPoints : public GUIElement
+{
+public:
+    Plot2DPoints() = delete;
+
+    Plot2DPoints(std::array<float,3> rgb);
+
+    ~Plot2DPoints() noexcept;
+
+    void draw() override;
+
+private:
+    size_t _npoints;
+
+    float* _points;
+
+    GLuint vertexbuffer;
+    GLuint position;
+};
+
+} // namespace vfps
+
+#endif // PLOT2DPOINTS_HPP
