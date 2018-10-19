72d5f53db213f999d3c9c9550eb93483277dc2e3
Patrik Schönfeldt <patrik@sedna.snft.de>
2018-10-19 21:21:27 +0200

    Improve readability of file headers
    * Replace verbose GPL header by SPDX license identifier
    * Replaced one remaining #include guard by pragma once
    
    Implements: #66 #76

--- inc/Z/CollimatorImpedance.hpp
+++ inc/Z/CollimatorImpedance.hpp
@@ -1,22 +1,9 @@
-/******************************************************************************
- * Inovesa - Inovesa Numerical Optimized Vlasov-Equation Solver Application   *
- * Copyright (c) 2017: Patrik Schönfeldt                                      *
- * Copyright (c) 2017: Karlsruhe Institute of Technology                      *
- *                                                                            *
- * This file is part of Inovesa.                                              *
- * Inovesa is free software: you can redistribute it and/or modify            *
- * it under the terms of the GNU General Public License as published by       *
- * the Free Software Foundation, either version 3 of the License, or          *
- * (at your option) any later version.                                        *
- *                                                                            *
- * Inovesa is distributed in the hope that it will be useful,                 *
- * but WITHOUT ANY WARRANTY; without even the implied warranty of             *
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              *
- * GNU General Public License for more details.                               *
- *                                                                            *
- * You should have received a copy of the GNU General Public License          *
- * along with Inovesa.  If not, see <http://www.gnu.org/licenses/>.           *
- ******************************************************************************/
+// SPDX-License-Identifier: GPL-3.0-or-later
+/*
+ * This file is part of Inovesa (github.com/Inovesa/Inovesa).
+ * It's copyrighted by the contributors recorded
+ * in the version control history of the file.
+ */
 
 #pragma once
 
