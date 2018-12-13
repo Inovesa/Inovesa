e96687ce93aec2aa308368d307a07d7c1ae9ea6f
Patrik Schönfeldt <patrik@prixima.snft.de>
2018-12-13 21:53:54 +0100

    Add simple test for status_string

--- test/TestMessageStrings.cpp
+++ test/TestMessageStrings.cpp
@@ -28,3 +28,11 @@ BOOST_AUTO_TEST_CASE(inovesa_version) {
     auto s = vfps::inovesa_version(true);
     longLinesCheck(s);
 }
+
+BOOST_AUTO_TEST_CASE(status_string) {
+    vfps::PhaseSpace::setSize(32,32,1);
+    auto ps = std::make_shared<vfps::PhaseSpace>(32,-1,1,3,-2,2,4,nullptr, 1,1);
+
+    auto s = vfps::status_string(ps,0,1);
+    longLinesCheck(s);
+}
