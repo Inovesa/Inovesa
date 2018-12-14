a014ed991cabcaa6f7272e45406a99e06bad6989
Patrik Schönfeldt <patrik@prixima.snft.de>
2018-12-14 23:02:03 +0100

    Add more tests

--- test/TestPhaseSpace.cpp
+++ test/TestPhaseSpace.cpp
@@ -6,10 +6,10 @@
 BOOST_AUTO_TEST_CASE( phasespace_constructors ){
     vfps::PhaseSpace::setSize(32,32,1);
     vfps::PhaseSpace ps(-12,12,3,-22,22,4,nullptr,1,1);
-    BOOST_CHECK(ps.getMax(0) ==  12);
-    BOOST_CHECK(ps.getMin(0) == -12);
-    BOOST_CHECK(ps.getMax(1) ==  22);
-    BOOST_CHECK(ps.getMin(1) == -22);
+    BOOST_CHECK_EQUAL(ps.getMax(0),  12);
+    BOOST_CHECK_EQUAL(ps.getMin(0), -12);
+    BOOST_CHECK_EQUAL(ps.getMax(1),  22);
+    BOOST_CHECK_EQUAL(ps.getMin(1), -22);
     BOOST_CHECK_CLOSE(ps.getIntegral(),
                       static_cast<vfps::integral_t>(1),
                       static_cast<vfps::integral_t>(0.1f));
--- test/TestRuler.cpp
+++ test/TestRuler.cpp
@@ -4,6 +4,15 @@
 #include <PS/Ruler.hpp>
 
 BOOST_AUTO_TEST_CASE( ruler_constructors ){
-    vfps::Ruler<vfps::meshaxis_t> axis(15,-4,4);
-    BOOST_CHECK(axis.zerobin() == 7);
+    vfps::Ruler<int32_t> axis(15,-4,4,{{"foo", 2.0}});
+    BOOST_CHECK_EQUAL(axis.zerobin(), 7);
+
+    // min > max
+    BOOST_CHECK_THROW( vfps::Ruler<int32_t>(7,3,-3),
+                       std::invalid_argument );
+
+    auto axis2(axis);
+    BOOST_CHECK_EQUAL(axis2.zerobin(), 7);
+    BOOST_CHECK_EQUAL(axis2.length(), 8);
+    BOOST_CHECK_EQUAL(axis2.scale("foo"), 2.0);
 }
