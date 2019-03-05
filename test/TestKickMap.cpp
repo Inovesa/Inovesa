a941cf2d80fbb8e04eace4340b7781291cb0473f
Patrik Schönfeldt <patrik@sedna.snft.de>
2019-03-05 22:18:25 +0100

    Add unit tests for KickMap::applyTo(Position&)

--- test/TestKickMap.cpp
+++ test/TestKickMap.cpp
@@ -14,16 +14,24 @@ BOOST_AUTO_TEST_CASE( linear_noclamp_map_x ){
     std::vector<vfps::integral_t> buckets{{1}};
     vfps::PhaseSpace::resetSize(4,buckets.size());
 
+    std::vector<vfps::PhaseSpace::Position> pos;
+    for(uint32_t i=0; i<9; i++){
+        pos.push_back({0.5f*i,0.5f*i});
+    }
+    std::vector<vfps::PhaseSpace::Position> pos2{
+        {1,0}, {1,0.5f}, {1,1}, {1,1.5f}, {1,2},
+        {1.5f,2.5f}, {3,3}, {3,3.5f}, {3,4}};
+
+    std::vector<vfps::meshaxis_t> offset{{ -1, 0.0, 1.0, 1.0 }};
+
     std::vector<vfps::meshdata_t> data1{{ 0.0, 0.1, 0.0, 1.0,
                                           0.0, 0.3, 0.7, 0.0,
                                           0.1, 0.7, 0.3, 0.0,
                                           0.9, 0.0, 0.0, 0.0 }};
 
-    std::vector<vfps::meshaxis_t> offset{{ -1, 1.0, 0.0, 1.0 }};
-
-    std::vector<vfps::meshdata_t> data2{{ 0.0, 0.3, 0.0, 0.0,
-                                          0.0, 0.7, 0.7, 0.0,
-                                          0.0, 0.0, 0.3, 0.0,
+    std::vector<vfps::meshdata_t> data2{{ 0.0, 0.1, 0.7, 0.0,
+                                          0.0, 0.3, 0.3, 0.0,
+                                          0.0, 0.7, 0.0, 0.0,
                                           0.1, 0.0, 0.0, 0.0 }};
 
     auto ps1 = std::make_shared<vfps::PhaseSpace>(-1,1,2,-1,1,4,nullptr,1,1,
@@ -40,6 +48,14 @@ BOOST_AUTO_TEST_CASE( linear_noclamp_map_x ){
     km.swapForce(offset);
 
     km.apply();
+    km.applyToAll(pos);
+
+    for (std::size_t i=0; i<pos.size(); i++) {
+        BOOST_CHECK_SMALL(pos[i].x-pos2[i].x,
+                          static_cast<vfps::meshaxis_t>(1e-5));
+        BOOST_CHECK_SMALL(pos[i].y-pos2[i].y,
+                          static_cast<vfps::meshaxis_t>(1e-5));
+    }
 
     for (auto i=0; i<16; i++) {
         BOOST_CHECK_SMALL(ps2->getData()[i]-data2[i],
@@ -51,16 +67,23 @@ BOOST_AUTO_TEST_CASE( linear_noclamp_map_y ){
     std::vector<vfps::integral_t> buckets{{1}};
     vfps::PhaseSpace::resetSize(4,buckets.size());
 
+    std::vector<vfps::PhaseSpace::Position> pos;
+    for(uint32_t i=0; i<9; i++){
+        pos.push_back({0.5f*i,0.5f*i});
+    }
+    std::vector<vfps::PhaseSpace::Position> pos2{
+        {0,1},{0.5f,1},{1,1},{1.5f,1},{2,1},{2.5f,1.5f},{3,3},{3.5f,3},{4,3}};
+
+    std::vector<vfps::meshaxis_t> offset{{ -1, 0.0, 1.0, 1.0 }};
+
     std::vector<vfps::meshdata_t> data1{{ 0.0, 0.1, 0.0, 0.3,
                                           0.0, 0.3, 0.7, 0.0,
                                           0.1, 0.7, 0.3, 0.0,
                                           0.5, 0.0, 0.6, 0.0 }};
 
-    std::vector<vfps::meshaxis_t> offset{{ -1, 1.0, 0.0, 1.0 }};
-
     std::vector<vfps::meshdata_t> data2{{ 0.0, 0.0, 0.1, 0.0,
-                                          0.3, 0.7, 0.0, 0.0,
-                                          0.1, 0.7, 0.3, 0.0,
+                                          0.0, 0.3, 0.7, 0.0,
+                                          0.7, 0.3, 0.0, 0.0,
                                           0.0, 0.6, 0.0, 0.0 }};
 
     auto ps1 = std::make_shared<vfps::PhaseSpace>(-1,1,2,-1,1,4,nullptr,1,1,
@@ -77,6 +100,15 @@ BOOST_AUTO_TEST_CASE( linear_noclamp_map_y ){
     km.swapForce(offset);
 
     km.apply();
+    km.applyToAll(pos);
+
+    for (std::size_t i=0; i<pos.size(); i++) {
+        BOOST_CHECK_SMALL(pos[i].x-pos2[i].x,
+                          static_cast<vfps::meshaxis_t>(1e-5));
+        BOOST_CHECK_SMALL(pos[i].y-pos2[i].y,
+                          static_cast<vfps::meshaxis_t>(1e-5));
+    }
+
 
     for (auto i=0; i<16; i++) {
         BOOST_CHECK_SMALL(ps2->getData()[i]-data2[i],
