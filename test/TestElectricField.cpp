2c8a42652b090541edffdb2a02a0851ded062473
Patrik Schönfeldt <patrik@sedna.snft.de>
2019-06-13 07:42:03 +0200

    Elaborate TestElectricField for multi-bunch tests

--- test/TestElectricField.cpp
+++ test/TestElectricField.cpp
@@ -4,6 +4,7 @@
  */
 
 #include <boost/test/unit_test.hpp>
+#include <boost/mpl/vector.hpp>
 
 #include <vector>
 
@@ -16,21 +17,38 @@
 
 
 struct ElectricFieldFixture {
-    ElectricFieldFixture()
-     : buckets(1, 0)
+    explicit ElectricFieldFixture(std::vector<vfps::integral_t> filling)
+      : filling(filling)
     {
+        for (uint32_t i=0; i<filling.size(); i++) {
+            if (filling[i] > 0) {
+                buckets.push_back(static_cast<uint32_t>(filling.size())-1-i);
+                bunches.emplace_back(filling[i]);
+            }
+        }
+        constexpr double bunchspacing = 4.0;
+
+        size_t spaced_bins = static_cast<size_t>(
+                    std::ceil(n*buckets.size()*bunchspacing));
+
         vfps::PhaseSpace::resetSize(n,1);
 
-        ps = std::make_shared<vfps::PhaseSpace>(-12,12,3,-12,12,4,nullptr,1,1);
-        z = std::make_shared<vfps::ConstImpedance>(n, 1e9, 1);
+        ps = std::make_shared<vfps::PhaseSpace>(
+                    -12,12,3,-12,12,4,nullptr,1,1,bunches);
+        z = std::make_shared<vfps::ConstImpedance>(spaced_bins, 1e9, 1);
 
         f = std::make_shared<vfps::ElectricField>(
-                    ps, z, buckets, 0, nullptr, 1e6, 0.1, 1e-3, 1e9, 1e3, 1e-3);
+                    ps, z, buckets, 0, nullptr,
+                    1e6, 0.1, 1e-3, 1e9, 1e3, 1e-3);
     }
 
     static constexpr uint32_t n = 32;
 
     std::vector<uint32_t> buckets;
+
+    std::vector<vfps::integral_t> bunches;
+
+    std::vector<vfps::integral_t> filling;
 
     std::shared_ptr<vfps::PhaseSpace> ps;
 
@@ -39,18 +57,35 @@ struct ElectricFieldFixture {
     std::shared_ptr<vfps::ElectricField> f;
 };
 
-
 constexpr uint32_t ElectricFieldFixture::n;
 
-BOOST_FIXTURE_TEST_SUITE( ElectricField, ElectricFieldFixture )
+struct ElectricFieldFixture1 : public ElectricFieldFixture{
+    ElectricFieldFixture1()
+      : ElectricFieldFixture(std::vector<vfps::integral_t>{1.0})
+    {}
+};
 
-BOOST_AUTO_TEST_CASE( constructors ){
-    BOOST_CHECK_EQUAL_COLLECTIONS(f->getBuckets().data(),
-                                  f->getBuckets().data()+f->getBuckets().size(),
-                                  buckets.data(),
-                                  buckets.data()+buckets.size());
+struct ElectricFieldFixture2 : public ElectricFieldFixture{
+    ElectricFieldFixture2()
+      : ElectricFieldFixture(std::vector<vfps::integral_t>{0.75, 0, 0.25})
+    {}
+};
 
-    // TODO: add more checks
+typedef boost::mpl::vector<ElectricFieldFixture1,
+                           ElectricFieldFixture2
+                          > ElectricFieldFixtures;
+
+
+BOOST_AUTO_TEST_SUITE( ElectricField )
+
+BOOST_FIXTURE_TEST_CASE_TEMPLATE(constructors , T, ElectricFieldFixtures, T)
+{
+    BOOST_CHECK_EQUAL_COLLECTIONS(T::f->getBuckets().data(),
+                                  T::f->getBuckets().data()
+                                  +T::f->getBuckets().size(),
+                                  T::buckets.data(),
+                                  T::buckets.data()
+                                  +T::buckets.size());
 }
 
 BOOST_AUTO_TEST_SUITE_END()
