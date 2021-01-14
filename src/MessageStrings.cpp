0bfffba680aff328dcc82b4819a13594ee7f9902
Patrick Schreiber <patrick-sch@gmx.de>
2021-01-14 15:56:05 +0100

    Release v1.1.1 hotfix
    closes #122

--- src/MessageStrings.cpp
+++ src/MessageStrings.cpp
@@ -14,17 +14,17 @@ const std::string vfps::copyright_notice() noexcept {
     std::string rv (
         "Inovesa Numerical Optimized Vlasov-Equation Solver Application\n"
         "\n");
-    rv+="Copyright (c) 2012-2018 Patrik Schönfeldt\n"
-        "Copyright (c) 2014-2018 Karlsruhe Institute of Technology\n"
-        "Copyright (c) 2017-2018 Johannes Schestag\n"
-        "Copyright (c) 2017 Tobias Boltz\n"
-        "Copyright (c) 2017 Patrick Schreiber\n"
-        "Copyright (c) 2017 Julian Gethmann\n"
-        "Copyright (c) 2017 Matthias Blaicher\n"
-        "Copyright (c) 1997-2016 John C. Bowman,\n"
+    rv+="Copyright (c) Patrik Schönfeldt\n"
+        "Copyright (c) Karlsruhe Institute of Technology\n"
+        "Copyright (c) Johannes Schestag\n"
+        "Copyright (c) Tobias Boltz\n"
+        "Copyright (c) Patrick Schreiber\n"
+        "Copyright (c) Julian Gethmann\n"
+        "Copyright (c) Matthias Blaicher\n"
+        "Copyright (c) John C. Bowman,\n"
         "\tUniversity of Alberta (Array class)\n"
         #if INOVESA_USE_OPENGL == 1
-        "Copyright (c) 2014-2015 Nathaniel J. Smith, Stefan van der Walt\n"
+        "Copyright (c) Nathaniel J. Smith, Stefan van der Walt\n"
         "\t (Magma color code)\n"
         #endif
         "\n"
